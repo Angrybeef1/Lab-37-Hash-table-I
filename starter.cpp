052b7802174ae22cc8bce301273ec6c6258bcaa3
Angrybeef1 <136150862+Angrybeef1@users.noreply.github.com>
2024-12-08 18:22:46 -0800

    PArt one Complete

--- starter.cpp
+++ starter.cpp
@@ -1,11 +1,33 @@
 // COMSC 210 | Lab 37 Hash Tables I | Robert Stonemetz
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int sum_ascii(string str){
+    int sum = 0;
+    for(char c : str){
+        sum += (int)c;
+    }
+    return sum;
+}
+
 
 int main() {
+    //test function
+    string test = "Hello";
+    int result = sum_ascii(test);
+
+    // Print result and show calculation
+    cout << "Testing sum_ascii() with string: " << test << endl;
+    cout << "Calculation breakdown:" << endl;
+    for(char c : test) {
+        cout << "ASCII value of '" << c << "' is " << (int)c << endl;
+    }
+    cout << "Total sum: " << result << endl;
+
+
     char a = 'A';
     cout << a << endl;
     cout << (int) a << endl;
